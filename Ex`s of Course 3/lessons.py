5e38cfc23c7d16f0357d9a4f85e4388bbb25e205
Ksorz <ksorzz@gmail.com>
2019-06-11 10:59:25 +0300

    11

--- Ex`s of Course 3/lessons.py
+++ Ex`s of Course 3/lessons.py
@@ -42,6 +42,7 @@ while True:
 mysock.close()
 # 12.4 - Retrieving Web Pages
 import urllib.request, urllib.parse, urllib.error
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt') # Parse the URL. what server to talk to, what document to retrieve, HTTP version, GET request, all that stuff. (Almost the same as open(file))
+print(fhand)
 for line in fhand:
-    print(line.decode().strip())
+    print(line.decode().strip()) # line is byte array (not str).
