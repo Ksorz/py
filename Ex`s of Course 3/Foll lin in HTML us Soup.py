5872b6fc6703c8b4fdced00dea5f4c5cbd6e3936
ksorz <ksorzz@gmail.com>
2019-06-14 16:52:22 +0300

    14

--- Ex`s of Course 3/Foll lin in HTML us Soup.py
+++ Ex`s of Course 3/Foll lin in HTML us Soup.py
@@ -1,15 +1,14 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
+import re
 
+_url = input('Enter URL: ')
+_count = int(input('Enter count: '))
+_position = int(input('Enter position: '))
+soup = BeautifulSoup(urllib.request.urlopen(_url), 'html.parser')
 
-url = input('Enter URL: ')
-_count = input('Enter count: ')
-_position = input('Enter position: ')
-html = urllib.request.urlopen('http://py4e-data.dr-chuck.net/known_by_Fikret.html').read()
-soup = BeautifulSoup(html, 'html.parser')
-tags = soup('a')
-print([tag.contents[i] for i in range(len(_count))])
+for i in range(_count + 1):
+    print(soup.find_all(string=re.compile('People that '), limit = 1))
+    soup = BeautifulSoup(urllib.request.urlopen(soup('a')[_position - 1].get('href')), 'html.parser')
 
-
-for tag in tags:
-    print(tag.contents[0])
+# .find_all(re.compile("that (\S.*\S)"))
